bde26ec3966194e567c7aff1b90ce0c92b1fb3f9
joaocorner <joaoeduardocorner@gmail.com>
2022-07-20 18:03:37 -0300

    home layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,25 @@
+import { FiLink } from 'react-icons/fi';
+import './home.css';
+
 export default function Home() {
     return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
+        <div className="container-home">
+
+            <div className="logo">
+                <img src="/logo-link2.png" alt="logo" />
+                <h1>My Links</h1>
+                <span>Paste your link to shorten 👇</span>
+            </div>
+
+            <div className="area-input">
+                <div>
+                    <FiLink size={24} color="#fff" />
+                    <input
+                        placeholder="Paste your link here..." />
+                </div>
+                <button>Shorten</button>
+            </div>
+
+        </div>
     )
-  }
+}
