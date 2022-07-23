0d4fef669f92704da753eb23e814edda2fedb72f
joaocorner <joaoeduardocorner@gmail.com>
2022-07-23 17:49:34 -0300

    added 404 layout

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,9 +1,14 @@
 import './error.css';
+import { Link } from 'react-router-dom';
 
 export default function Error() {
     return (
-        <div>
-            <h1>404 - Page not found</h1>
+        <div className='container-error'>
+            <img src="notfound.png" alt="Page not found" />
+            <h1>Page not found! 404</h1>
+            <Link to="/">
+                Back to Home
+            </Link>
         </div>
     )
 }
