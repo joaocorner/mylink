e4bd224649fbfbabe34a82b89dbf77db5efa7259
joaocorner <joaoeduardocorner@gmail.com>
2022-07-23 18:31:56 -0300

    get what was written to useState link

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { FiLink } from 'react-icons/fi';
 import './home.css';
 
 import Menu from '../../components/Menu';
 
 export default function Home() {
+    const [link, setLink] = useState('');
+
     return (
         <div className="container-home">
 
@@ -17,11 +20,14 @@ export default function Home() {
                 <div>
                     <FiLink size={24} color="#fff" />
                     <input
-                        placeholder="Paste your link here..." />
+                        placeholder="Paste your link here..."
+                        value={link}
+                        onChange={  (e) => setLink(e.target.value)}
+                    />
                 </div>
                 <button>Shorten</button>
             </div>
-        <Menu/>
+            <Menu />
         </div>
     )
 }
