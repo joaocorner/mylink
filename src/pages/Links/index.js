113dd4b7c3461c0bd8ca75a6863b9691b3648e3c
joaocorner <joaoeduardocorner@gmail.com>
2022-07-24 14:22:20 -0300

    click on the link and it shows the updated modal

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -4,6 +4,7 @@ import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 import { getLinksSave } from '../services/storeLinks';
+import LinkItem from '../../components/LinkItem';
 
 import Menu from '../../components/Menu';
 
@@ -30,6 +31,11 @@ export default function Links() {
 
   }, []);
 
+  function handleOpenLink(link) {
+    setData(link);
+    setShowModal(true);
+  }
+
   return (
     <div className='links-container'>
       <div className='links-header'>
@@ -43,7 +49,9 @@ export default function Links() {
         <div
           key={link.id}
           className='links-item'>
-          <button className='link'>
+          <button
+            className='link'
+            onClick={() => handleOpenLink(link)}>
             <FiLink size={18} color='#fff' />
             {link.long_url}
           </button>
@@ -52,6 +60,13 @@ export default function Links() {
           </button>
         </div>
       ))}
+
+      {showModal && (<LinkItem
+        closeModal={() => setShowModal(false)}
+        content={data}
+      />
+      )}
+
     </div>
   )
 }
